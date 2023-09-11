baa4050459c55dd03fe48b959ea7b665cc83d8f9
abdalla-3077 <abd010241@gmail.com>
2023-09-11 16:27:49 +0300

    Pro Db Plus

---
+++ index.js
@@ -0,0 +1,324 @@
+const fs = require('fs')
+let fileName
+const Cryptr = require('cryptr');
+class Data {
+    /**
+     * 
+     * @param {Object} options 
+     * @param {string} options.fileName
+     */
+    constructor(options = { fileName }) {
+        if (options.fileName) fileName = options.fileName || `database.json`
+        this.fileName = options.fileName || `database.json`
+    }
+
+
+
+    setEncrypted(data, UnEncryptedValue, code) {
+        let SecretKey
+        if (!code) {
+            SecretKey = "pMzzkg1T3mlTj2k=VeyLZYQrq9V2rcvXBaJwmDXe-XvO6h5sUraP=RnxdMb3wcYA"
+            console.warn("[Pro Db Plus] - Your secret key is the default one, and it's not safe.")
+        } else {
+            SecretKey = code
+        }
+        const cryptr = new Cryptr(SecretKey);
+        const value = cryptr.encrypt(UnEncryptedValue);
+
+        if (!fs.existsSync(`${this.fileName}`)) {
+            fs.writeFileSync(`${this.fileName}`, `{}`);
+
+            if (!data) return false;
+            const file = JSON.parse(fs.readFileSync(`${this.fileName}`, `utf8`))
+            file[data] = value
+            return fs.writeFileSync(`${this.fileName}`, JSON.stringify(file, null, 1))
+        } else {
+            if (!data) return false;
+            const file = JSON.parse(fs.readFileSync(`${this.fileName}`, `utf8`))
+            file[data] = value
+            return fs.writeFileSync(`${this.fileName}`, JSON.stringify(file, null, 1))
+        }
+    }
+
+    getEncrypted(data, code) {
+        const secretKey = code || "pMzzkg1T3mlTj2k=VeyLZYQrq9V2rcvXBaJwmDXe-XvO6h5sUraP=RnxdMb3wcYA";
+        const cryptr = new Cryptr(secretKey);
+
+        if (!fs.existsSync(this.fileName)) {
+            fs.writeFileSync(this.fileName, '{}');
+        }
+
+        if (!data) return false;
+
+        const file = JSON.parse(fs.readFileSync(this.fileName, 'utf8'));
+        const encryptedData = file[data];
+
+        if (!encryptedData) return false;
+
+        try {
+            const decryptedValue = cryptr.decrypt(encryptedData);
+            return decryptedValue;
+        } catch (error) {
+            console.error("[Pro Db Plus] - \n", error);
+            return null;
+        }
+    }
+
+
+    set(data, value) {
+        if (!fs.existsSync(`${this.fileName}`)) {
+            fs.writeFileSync(`${this.fileName}`, `{}`);
+
+            if (!data) return false;
+            const file = JSON.parse(fs.readFileSync(`${this.fileName}`, `utf8`))
+            file[data] = value
+            return fs.writeFileSync(`${this.fileName}`, JSON.stringify(file, null, 1))
+        } else {
+            if (!data) return false;
+            const file = JSON.parse(fs.readFileSync(`${this.fileName}`, `utf8`))
+            file[data] = value
+            return fs.writeFileSync(`${this.fileName}`, JSON.stringify(file, null, 1))
+        }
+    }
+
+    fetch(data) {
+        if (!fs.existsSync(`${this.fileName}`)) {
+            fs.writeFileSync(`${this.fileName}`, `{}`);
+
+            if (!data) return false;
+            const file = JSON.parse(fs.readFileSync(`${this.fileName}`, `utf8`))
+            return file[data]
+        } else {
+            if (!data) return false;
+            const file = JSON.parse(fs.readFileSync(`${this.fileName}`, `utf8`))
+            return file[data]
+        }
+    }
+    has(data) {
+        if (!fs.existsSync(`${this.fileName}`)) {
+            fs.writeFileSync(`${this.fileName}`, `{}`);
+            if (!data) return false;
+            const file = JSON.parse(fs.readFileSync(`${this.fileName}`, `utf8`))
+            return file[data] ? true : false
+        } else {
+            if (!data) return false;
+            const file = JSON.parse(fs.readFileSync(`${this.fileName}`, `utf8`))
+            return file[data] ? true : false
+        }
+    }
+    fetchAll() {
+        const fetchall = JSON.parse(fs.readFileSync(`${this.fileName}`, `utf8`))
+        return fetchall
+    }
+    delete(data) {
+        if (!fs.existsSync(`${this.fileName}`)) {
+            fs.writeFileSync(`${this.fileName}`, `{}`);
+            if (!data) return false;
+            const file = JSON.parse(fs.readFileSync(`${this.fileName}`, `utf8`))
+            if (!file[data]) throw new TypeError('Theres no data to add to database! \n' + __dirname)
+            delete file[data]
+            return fs.writeFileSync(`${this.fileName}`, JSON.stringify(file, null, 1))
+        } else {
+            if (!data) return false;
+            const file = JSON.parse(fs.readFileSync(`${this.fileName}`, `utf8`))
+            if (!file[data]) throw new TypeError('Theres no data to add to database! \n' + __dirname)
+            delete file[data]
+            return fs.writeFileSync(`${this.fileName}`, JSON.stringify(file, null, 1))
+        }
+    }
+    backup(fileName) {
+        if (!fs.existsSync(`${this.fileName}`)) {
+            fs.writeFileSync(`${this.fileName}`, `{}`);
+            if (!fileName) throw new TypeError(`Filename not defined!`)
+            const file = JSON.parse(fs.readFileSync(`${this.fileName}`, `utf8`))
+            return fs.writeFileSync(`${this.fileName}.json`, JSON.stringify(file, null, 1))
+        } else {
+            if (!fileName) throw new TypeError(`Filename not defined!`)
+            const file = JSON.parse(fs.readFileSync(`${this.fileName}`, `utf8`))
+            return fs.writeFileSync(`${this.fileName}.json`, JSON.stringify(file, null, 1))
+        }
+    }
+    add(data, value) {
+        if (!fs.existsSync(`${this.fileName}`)) {
+            fs.writeFileSync(`${this.fileName}`, `{}`);
+            if (!data) return false;
+            if (isNaN(value)) throw new TypeError(`The value must be a number!`)
+            if (!this.has(data)) {
+                this.set(data, 0)
+            }
+
+            const file = JSON.parse(fs.readFileSync(`${this.fileName}`, `utf8`))
+            file[data] += value
+            return fs.writeFileSync(`${this.fileName}`, JSON.stringify(file, null, 1))
+        } else {
+            if (!data) return false;
+            if (isNaN(value)) throw new TypeError(`The value must be a number!`)
+            if (!this.has(data)) {
+                this.set(data, 0)
+            }
+            const file = JSON.parse(fs.readFileSync(`${this.fileName}`, `utf8`))
+            file[data] += value
+            return fs.writeFileSync(`${this.fileName}`, JSON.stringify(file, null, 1))
+
+        }
+    }
+    subtract(data, value) {
+        if (!fs.existsSync(`${this.fileName}`)) {
+            fs.writeFileSync(`${this.fileName}`, `{}`);
+            if (!data) return false;
+            if (typeof value !== `number`) throw new TypeError(`The value must be a number!`)
+            if (!this.has(data)) throw new TypeError(`The data is not defined!`)
+            if (typeof this.fetch(data) !== `number`) throw new TypeError(`The value must be number!`)
+            const file = JSON.parse(fs.readFileSync(`${this.fileName}`, `utf8`))
+            file[data] -= value
+            return fs.writeFileSync(`${this.fileName}`, JSON.stringify(file, null, 1))
+        } else {
+            if (!data) return false;
+            if (typeof value !== `number`) throw new TypeError(`The value must be a number!`)
+            if (!this.has(data)) throw new TypeError(`The data is not defined!`)
+            if (typeof this.fetch(data) !== `number`) throw new TypeError(`The value must be number!`)
+            const file = JSON.parse(fs.readFileSync(`${this.fileName}`, `utf8`))
+            file[data] -= value
+            return fs.writeFileSync(`${this.fileName}`, JSON.stringify(file, null, 1))
+        }
+    }
+    reset() {
+        const file = JSON.parse(fs.readFileSync(`${this.fileName}`, `utf8`))
+        return fs.writeFileSync(`${this.fileName}`, JSON.stringify({}, null, 1))
+    }
+    all(sınır = 0) {
+        if (!fs.existsSync(`${this.fileName}`)) {
+            fs.writeFileSync(`${this.fileName}`, `{}`);
+            const object = JSON.parse(fs.readFileSync(`${this.fileName}`, `utf8`));
+
+            var result = [];
+            for (var i in object)
+                result.push([`DATA = ${i}`, `VALUE = ${object[i]}`]);
+            return result
+        } else {
+            const object = JSON.parse(fs.readFileSync(`${this.fileName}`, `utf8`));
+
+            var result = [];
+            for (var i in object)
+                result.push([`DATA = ${i}`, `VALUE = ${object[i]}`]);
+            return result
+        }
+    }
+    push(key, value) {
+        if (!fs.existsSync(`${this.fileName}`)) {
+            fs.writeFileSync(`${this.fileName}`, `{}`);
+            const veri = this.fetch(key);
+            if (!veri) {
+
+                return this.set(key, [value]);
+            }
+            if (Array.isArray(veri)) {
+                veri.push(value);
+                return this.set(key, veri);
+            } else {
+
+                return this.set(key, [value]);
+            }
+        } else {
+            const veri = this.fetch(key);
+            if (!veri) {
+
+                return this.set(key, [value]);
+            }
+            if (Array.isArray(veri)) {
+                veri.push(value);
+                return this.set(key, veri);
+            } else {
+
+                return this.set(key, [value]);
+            }
+        }
+    }
+    math(data, limit, value) {
+        if (!fs.existsSync(`${this.fileName}`)) {
+            fs.writeFileSync(`${this.fileName}`, `{}`);
+            const file = JSON.parse(fs.readFileSync(`${this.fileName}`, `utf8`))
+            if (!data) throw new TypeError(`Theres no data founded!`)
+            if (!limit) throw new TypeError(`The process not defined!`)
+            if (!value) throw new TypeError(`Theres no value entered!`)
+            if (!file[data]) throw new TypeError('couldnt get the data!')
+            if (isNaN(value)) throw new TypeError(`The value must be number!`)
+            if (isNaN(this.fetch(data))) throw new TypeError(`The value must be number!`)
+            switch (limit) {
+                case `+`:
+                    file[data] += value
+                    return fs.writeFileSync(`${this.fileName}`, JSON.stringify(file, null, 1))
+                    break;
+                case `-`:
+                    file[data] -= value
+                    return fs.writeFileSync(`${this.fileName}`, JSON.stringify(file, null, 1))
+                    break;
+                case `*`:
+                    file[data] *= value
+                    return fs.writeFileSync(`${this.fileName}`, JSON.stringify(file, null, 1))
+                    break;
+                case `/`:
+                    file[data] /= value
+                    return fs.writeFileSync(`${this.fileName}`, JSON.stringify(file, null, 1))
+                    break;
+                case `%`:
+                    file[data] %= value
+                    return fs.writeFileSync(`${this.fileName}`, JSON.stringify(file, null, 1))
+                    break;
+                default:
+                    return undefined
+                    break;
+            }
+        } else {
+            const file = JSON.parse(fs.readFileSync(`${this.fileName}`, `utf8`))
+            if (!data) throw new TypeError(`Theres no data founded!`)
+            if (!limit) throw new TypeError(`The process not defined!`)
+            if (!value) throw new TypeError(`Theres no value entered!`)
+            if (!file[data]) throw new TypeError('couldnt get the data!')
+            if (isNaN(value)) throw new TypeError(`The value must be number!`)
+            if (isNaN(this.fetch(data))) throw new TypeError(`The data must be number!`)
+            switch (limit) {
+                case `+`:
+                    file[data] += value
+                    return fs.writeFileSync(`${this.fileName}`, JSON.stringify(file, null, 1))
+                    break;
+                case `-`:
+                    file[data] -= value
+                    return fs.writeFileSync(`${this.fileName}`, JSON.stringify(file, null, 1))
+                    break;
+                case `*`:
+                    file[data] *= value
+                    return fs.writeFileSync(`${this.fileName}`, JSON.stringify(file, null, 1))
+                    break;
+                case `/`:
+                    file[data] /= value
+                    return fs.writeFileSync(`${this.fileName}`, JSON.stringify(file, null, 1))
+                    break;
+                case `%`:
+                    file[data] %= value
+                    return fs.writeFileSync(`${this.fileName}`, JSON.stringify(file, null, 1))
+                    break;
+                default:
+                    return undefined
+                    break;
+            }
+        }
+
+
+    }
+
+    get(data) {
+        if (!fs.existsSync(`${this.fileName}`)) {
+            fs.writeFileSync(`${this.fileName}`, `{}`);
+
+            if (!data) return false;
+            const file = JSON.parse(fs.readFileSync(`${this.fileName}`, `utf8`))
+            return file[data]
+        } else {
+            if (!data) return false;
+            const file = JSON.parse(fs.readFileSync(`${this.fileName}`, `utf8`))
+            return file[data]
+        }
+    }
+}
+module.exports.DB = Data
---
+++ readme.md
@@ -0,0 +1,56 @@
+<div  align="center">
+<p>
+<img style="margin-bottom:-6px" src="https://i.ibb.co/gttdg4C/68747470733a2f2f6d656469612e646973636f72646170702e6e65742f6174746163686d656e74732f393135393539393731.png"/>
+<a href="https://www.npmjs.com/package/pro.db-plus/" alt="pro.db-fork" /></a>
+</p>
+</p>
+</div>
+
+## What is pro.db-plus ?
+- It is a easy and quick storage unit that relies on `objects` to store data in **JSON** format
+- BTW THIS IS A FORK ( NOT REAL VERSION ) check it out here [https://www.npmjs.com/package/pro.db/]
+- Encrypt-Decrypt data set with SecretKey you can generate SecretKey from [https://generate-random.org/api-token-generator?count=1&length=64&type=mixed-numbers-symbols]
+
+## Installation
+- You need to install the package on your project
+```sh-session
+npm install pro.db-plus
+yarn add pro.db-plus
+```
+## Important Alert ⚠
+
+- The primary secret key is not secure. It is preferable to choose your own secret key
+
+## How To Use
+```js
+const {DB} = require('pro.db-plus');
+
+const db = new DB({fileName: `database.json`})
+
+await db.set('key','value');// to set a data to database.
+await db.get('key');// to get the data by key.
+await db.delete('key');// to delete key from database.
+await db.has('key','value');// return "true" or "false".
+
+await db.setEncrypted('key','value','code');// to set a data to database. but its Encrypted 
+await db.setEncrypted('key','code');// to get the Encrypted data by key.
+
+
+await db.add('key', 10;);// to add a number to the key.
+await db.substract('key', 5);// to subtract a number from the key.
+await db.push('key', 10);// to set a data at the end.
+await db.math("key","+",5);// to math the numbers.
+
+await db.fetch(); // to fetch the data from database. 
+await db.fetchAll();// to fetchAll data.
+await db.all();// to get all data in database.
+
+await db.backup("Filename");// to make a backup file.
+await db.reset();// to delete all data and database.
+
+
+
+```
+## Contact
+
+- Contact With Me Discord : [`.__.abdalla`](https://www.npmjs.com/package/pro.db-plus)
